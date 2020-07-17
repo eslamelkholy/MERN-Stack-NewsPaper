af3a59552389836628675066256078e0ed09796e
eslam <eslamelkholy5325762@gmail.com>
2020-07-17 23:45:01 +0200

    Finished News Project Back-end Time For React With Redux

--- components/favorites/favorites.controller.js
+++ components/favorites/favorites.controller.js
@@ -1,13 +1,18 @@
 const { User } = require("../../models");
 
-exports.addUserFavorites = async (req, res) => {
+const addUserFavorites = async (req, res) => {
   const user = await User.findByPk(req.user.id);
   await user.addFavorites(req.body.articleId);
   res.status(201).json({ message: "Article Added Successfully" });
 };
 
-exports.removeUserFavorites = async (req, res) => {
+const removeUserFavorites = async (req, res) => {
   const user = await User.findByPk(req.user.id);
   await user.removeFavorites(req.body.articleId);
   res.status(201).json({ message: "Article Removed Successfully" });
+};
+
+module.exports = {
+  addUserFavorites,
+  removeUserFavorites,
 };
--- components/favorites/request/FavoritesMiddleware.js
+++ components/favorites/request/FavoritesMiddleware.js
@@ -1,9 +1,11 @@
 const { Article } = require("../../../models");
 
+const checkIfArticleExists = async (req, res, next) => {
+  const article = await Article.findByPk(req.body.articleId);
+  if (!article) return res.status(400).json({ message: "Article Not Found!!" });
+  next();
+};
+
 module.exports = {
-  checkIfArticleExists: async (req, res, next) => {
-    const article = await Article.findByPk(req.body.articleId);
-    if (!article) return res.status(400).json({ message: "Article Not Found!!" });
-    next();
-  },
+  checkIfArticleExists,
 };
